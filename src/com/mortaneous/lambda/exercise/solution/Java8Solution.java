e5a0f62bd2597249a94278075013a79027fe5481
Mortaneous <mortaneous.git@gmail.com>
2018-11-11 00:17:36 +0800

    Added Java 8 solution using lambda expressions

--- src/com/mortaneous/lambda/exercise/solution/Java8Solution.java
+++ src/com/mortaneous/lambda/exercise/solution/Java8Solution.java
@@ -2,7 +2,9 @@ package com.mortaneous.lambda.exercise.solution;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 import com.mortaneous.lambda.exercise.Person;
 
@@ -30,12 +32,27 @@ public class Java8Solution {
 		System.out.println("=========[Java8 solution]=========");
 		
 		// Task 1. Write code that will sort a list of Person objects by last name alphabetically 
+		Collections.sort(people, (p1,p2) -> p1.getLastName().compareTo(p2.getLastName()));
 		
 		// Task 2. Write a method that displays all objects in the list
+		System.out.println("*** Sort by last name:");
+		printWithCondition(people, p -> true);
 		
 		// Task 3. Write a method that displays Persons whose last names begin with C
+		System.out.println("*** People whose last name begin with C:");
+		printWithCondition(people, p -> p.getLastName().startsWith("C"));
 		
 		// Task 4. Write a method that displays Persons older than 30	}
+		System.out.println("*** People older than 30:");
+		printWithCondition(people, p -> p.getAge() > 30);
+	}
+
+	private static void printWithCondition(List<Person> people, Predicate<Person> predicate) {
+		for(Person p : people) {
+			if(predicate.test(p)) {
+				System.out.println(p);
+			}
+		}
 	}
 
 }
