94b7d74ffd2abc75dd81da91b1ea7b722b1fd092
Mortaneous <mortaneous.git@gmail.com>
2018-11-11 00:09:08 +0800

    Added Java 7 solution

--- src/com/mortaneous/lambda/exercise/solution/Java7Solution.java
+++ src/com/mortaneous/lambda/exercise/solution/Java7Solution.java
@@ -2,6 +2,8 @@ package com.mortaneous.lambda.exercise.solution;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import com.mortaneous.lambda.exercise.Person;
@@ -30,12 +32,52 @@ public class Java7Solution {
 		System.out.println("=========[Java7 solution]=========");
 		
 		// Task 1. Write code that will sort a list of Person objects by last name alphabetically
+		Collections.sort(people, new Comparator<Person>() {
+			@Override
+			public int compare(Person o1, Person o2) {
+				return o1.getLastName().compareTo(o2.getLastName());
+			}
+		});
 		
 		// Task 2. Write a method that displays all objects in the list
+		System.out.println("*** Sort by last name:");
+		printAll(people);
 		
 		// Task 3. Write a method that displays Persons whose last names begin with C
+		System.out.println("*** People whose last name begin with C:");
+		printWithCondition(people, new Condition<Person>() {
+			@Override
+			public boolean test(Person t) {
+				return t.getLastName().startsWith("C");
+			}
+		});
 		
 		// Task 4. Write a method that displays Persons older than 30
+		System.out.println("*** People older than 30:");
+		printWithCondition(people, new Condition<Person>() {
+			@Override
+			public boolean test(Person t) {
+				return t.getAge()>30;
+			}
+		});
+	}
+
+	private static void printWithCondition(List<Person> people, Condition<Person> condition) {
+		for(Person p : people) {
+			if(condition.test(p)) {
+				System.out.println(p);
+			}
+		}
+	}
+
+	private static void printAll(List<Person> people) {
+		for(Person p : people) {
+			System.out.println(p);
+		}
+	}
+	
+	private interface Condition<T> {
+		public boolean test(T t);
 	}
 
 }
