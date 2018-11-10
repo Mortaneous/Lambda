1842f8ea52757d766dcbf30ce7cb4dc2a98700b8
Mortaneous <mortaneous.git@gmail.com>
2018-11-10 23:50:59 +0800

    Additional preparatory code

--- src/com/mortaneous/lambda/exercise/ExcerciseDetails.java
+++ src/com/mortaneous/lambda/exercise/ExcerciseDetails.java
@@ -1,9 +1,25 @@
 package com.mortaneous.lambda.exercise;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ExcerciseDetails {
 
 	public static void main(String[] args) {
 
+		List<Person> people = new ArrayList<>(
+			Arrays.asList(
+				new Person("Smith", "John", 32),
+				new Person("Croft", "Lara", 29),
+				new Person("McCalister", "Kevin", 10),
+				new Person("Markson", "John", 25),
+				new Person("Chaplin", "Charlie", 65),
+				new Person("Johnson", "Mark", 30),
+				new Person("Dumbledore", "Albus Wolfric Brian", 72)
+			)
+		);
+		
 		// Task 1. Write code that will sort a list of Person objects by last name alphabetically 
 		
 		// Task 2. Write a method that displays all objects in the list
@@ -11,7 +27,7 @@ public class ExcerciseDetails {
 		// Task 3. Write a method that displays Persons whose last names begin with C
 		
 		// Task 4. Write a method that displays Persons older than 30
-		
+
 	}
 
 }
--- src/com/mortaneous/lambda/exercise/solution/Java8Solution.java
+++ src/com/mortaneous/lambda/exercise/solution/Java8Solution.java
@@ -1,9 +1,41 @@
 package com.mortaneous.lambda.exercise.solution;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.mortaneous.lambda.exercise.Person;
+
 public class Java8Solution {
 
 	public static void main(String[] args) {
-		System.out.println("Java8 solution");
+
+		System.out.println("=========[Initial state ]=========");
+
+		List<Person> people = new ArrayList<>(
+				Arrays.asList(
+					new Person("Smith", "John", 32),
+					new Person("Croft", "Lara", 29),
+					new Person("McCalister", "Kevin", 10),
+					new Person("Markson", "John", 25),
+					new Person("Chaplin", "Charlie", 65),
+					new Person("Johnson", "Mark", 30),
+					new Person("Dumbledore", "Albus Wolfric Brian", 72)
+				)
+			);
+		for(Person person : people) {
+			System.out.println(person);
+		}
+
+		System.out.println("=========[Java8 solution]=========");
+		
+		// Task 1. Write code that will sort a list of Person objects by last name alphabetically 
+		
+		// Task 2. Write a method that displays all objects in the list
+		
+		// Task 3. Write a method that displays Persons whose last names begin with C
+		
+		// Task 4. Write a method that displays Persons older than 30	}
 	}
 
 }
